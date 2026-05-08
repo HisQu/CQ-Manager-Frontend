6e847a446036146e2f286bddf592c27d0fe813d1
Friedrich Answin Daniel Motz <hello@danielmotz.de>
2026-05-08 13:52:11 +0200

    fix: LandingView login link

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -115,9 +115,9 @@ const mobileMenuOpen = ref(false)
                   }}</a>
               </div>
               <div class="py-6">
-                <a href="#"
+                <RouterLink to="/login" @click="mobileMenuOpen = false"
                    class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log
-                  in</a>
+                  in</RouterLink>
               </div>
             </div>
           </div>
