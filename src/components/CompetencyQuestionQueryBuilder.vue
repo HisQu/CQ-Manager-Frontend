9a8486622af51e1e4e8602e966c8902e629fab1c
Friedrich Answin Daniel Motz <hello@danielmotz.de>
2026-05-12 16:35:20 +0200

    fix: move SPARQL query to the bottom

--- src/components/CompetencyQuestionQueryBuilder.vue
+++ src/components/CompetencyQuestionQueryBuilder.vue
@@ -186,21 +186,6 @@ fetchTerms()
       </div>
     </div>
 
-    <div class="mt-6">
-      <label for="sparqlQuery"
-             class="block text-sm font-medium leading-6 dark:text-gray-200 text-gray-900">SPARQL Query</label>
-      <div class="mt-2">
-        <SparqlEditor v-if="canEdit"
-                      v-model="localSparqlQuery"
-        />
-        <SparqlEditor v-else-if="props.sparqlQuery"
-                      :model-value="props.sparqlQuery"
-                      :readonly="true"
-        />
-        <p v-else class="text-sm text-gray-400 dark:text-gray-500 italic">No SPARQL query defined.</p>
-      </div>
-    </div>
-
     <h3 class="mt-10 mb-5 text-xl">Term Annotations</h3>
     <p class="mt-2 text-sm text-gray-700 dark:text-gray-300 mb-5">
       This section allows you to add terms to a question. If a certain term does not exist yet, it will be created.
@@ -330,6 +315,21 @@ fetchTerms()
                 class="inline-flex items-center gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
           <CheckCircleIcon class="-ml-0.5 h-5 w-5" aria-hidden="true"/>
         </button>
+      </div>
+    </div>
+
+    <div class="mt-6">
+      <label for="sparqlQuery"
+             class="block text-sm font-medium leading-6 dark:text-gray-200 text-gray-900">SPARQL Query</label>
+      <div class="mt-2">
+        <SparqlEditor v-if="canEdit"
+                      v-model="localSparqlQuery"
+        />
+        <SparqlEditor v-else-if="props.sparqlQuery"
+                      :model-value="props.sparqlQuery"
+                      :readonly="true"
+        />
+        <p v-else class="text-sm text-gray-400 dark:text-gray-500 italic">No SPARQL query defined.</p>
       </div>
     </div>
   </div>
